702a2135b98015e40b2d7e7fc7a4c6bda9d2aeae
Jason Caetano <jasonc101@gmail.com>
2015-01-02 01:34:55 -0500

    added light/sound sensor and slowed accel sensor output

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var gpio = tessel.port['GPIO'];
 var accel = require('accel-mma84').use(tessel.port.A);
 var _ = require('lodash');
 var winston = require('winston');
+var ambient = require('ambient-attx4').use(tessel.port.B);
+var async = require('async');
 
 var led1 = tessel.led[0].output(1);
 var led2 = tessel.led[1].output(0);
@@ -17,7 +19,24 @@ var vibrateMotor = gpio.pwm[0];
 vibrateMotor.pwmDutyCycle(0.5);
 
 accel.on('ready', function() {
+  accel.setOutputRate(1.56);
   accel.on('data', function(readout) {
     winston.info(readout);
   });
+});
+
+ambient.on('ready', function() {
+  setInterval(function() {
+    async.parallel({
+      light: function(cb) {
+        ambient.getLightLevel(cb);
+      },
+      sound: function(cb) {
+        ambient.getSoundLevel(cb);
+      }
+    }, function(err, data) {
+      if (err) winston.error(err);  
+      winston.info(data);
+    });
+  }, 1000);
 });
