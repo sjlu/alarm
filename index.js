24473e94b0fd152ee9867e87a80beb49f434e235
Jason Caetano <jasonc101@gmail.com>
2015-01-02 01:20:33 -0500

    logging accel

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var tessel = require('tessel');
 var gpio = tessel.port['GPIO'];
+var accel = require('accel-mma84').use(tessel.port.A);
+var _ = require('lodash');
+var winston = require('winston');
 
 var led1 = tessel.led[0].output(1);
 var led2 = tessel.led[1].output(0);
@@ -12,3 +15,9 @@ setInterval(function() {
 gpio.pwmFrequency(1);
 var vibrateMotor = gpio.pwm[0];
 vibrateMotor.pwmDutyCycle(0.5);
+
+accel.on('ready', function() {
+  accel.on('data', function(readout) {
+    winston.info(readout);
+  });
+});
