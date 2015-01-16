3babe83377d919f94d377f87c8c246911bfa8a80
Steven Lu <tacticalazn@gmail.com>
2015-01-15 19:12:05 -0500

    Update alarm.ino

--- alarm.ino
+++ alarm.ino
@@ -1,12 +1,29 @@
-// beginning of the alarm code
+// pressure seems to range anywhere from
+// 0 - 3500? Dividing this number by 10
+// will give us a range from 0 - 350, giving
+// us a more tolerant reading
 int pressure = 0;
+
+// the amount in which a person is in bed or not
+int threshold = 200;
+
+// this tells us if the person is in
+// bed or not
+int in_bed = 0;
 
 void setup() {
   Spark.variable("pressure", &pressure, INT);
+  Spark.variable("in_bed", &in_bed, INT);
 }
 
 void loop() {
 
-  pressure = analogRead(A0);
+  pressure = analogRead(A0) / 10;
+
+  if (pressure > threshold) {
+    in_bed = 1;
+  } else {
+    in_bed = 0;
+  }
 
 }
