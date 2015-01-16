a9d0ce3e5ccb06de1463991b2d99f14e4087bf40
Steven Lu <tacticalazn@gmail.com>
2015-01-16 00:10:25 -0500

    implement a delay so we don't register too many actions

--- alarm.ino
+++ alarm.ino
@@ -1,35 +1,51 @@
+// the amount in which a person is in bed or not
+int THRESHOLD = 80;
+
+// how long should we delay before we
+// register an action?
+int DELAY_MIN = 600;
+
 // pressure seems to range anywhere from
 // 0 - 3500? Dividing this number by 35
 // will give us a range from 0 - 100, giving
 // us a more tolerant reading
 int pressure = 0;
 
-// the amount in which a person is in bed or not
-int threshold = 80;
-
 // this tells us if the person is in
 // bed or not
 boolean in_bed = false;
 
+// the loop is too fast and if someone
+// moves in the bed, it'll cause a momentary
+// on/off in in_bed and send junk signals,
+// therefore we will do a momentary delay
+// to see if they cross the threshold again
+int delay_time = 0;
+
 void setup() {
+  // register variables to be red
+  // by the hub
   Spark.variable("pressure", &pressure, INT);
   Spark.variable("in_bed", &in_bed, BOOLEAN);
 }
 
+// loop runs about every 5-15ms
 void loop() {
 
+  // read the input
   pressure = analogRead(A0) / 35;
 
-  if (pressure > threshold) {
-    if (!in_bed) {
-      Spark.publish("alarm/is_in_bed", "true", 60, PRIVATE);
-    }
-    in_bed = true;
+  boolean in_bed_read = pressure > THRESHOLD;
+  if (in_bed_read != in_bed) {
+    delay_time = delay_time + 1;
   } else {
-    if (in_bed) {
-      Spark.publish("alarm/is_in_bed", "false", 60, PRIVATE);
-    }
-    in_bed = false;
+    delay_time = 0;
+  }
+
+  if (delay_time > DELAY_MIN) {
+    in_bed = in_bed_read;
+    Spark.publish("alarm/is_in_bed", in_bed ? "true" : "false", 60, PRIVATE);
+    delay_time = 0;
   }
 
 }
