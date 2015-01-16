75b91954a8f11c54c22506e57fb87dfa8d346cd6
Steven Lu <tacticalazn@gmail.com>
2015-01-15 21:49:47 -0500

    publish if user is in bed

--- alarm.ino
+++ alarm.ino
@@ -1,29 +1,35 @@
 // pressure seems to range anywhere from
-// 0 - 3500? Dividing this number by 10
-// will give us a range from 0 - 350, giving
+// 0 - 3500? Dividing this number by 35
+// will give us a range from 0 - 100, giving
 // us a more tolerant reading
 int pressure = 0;
 
 // the amount in which a person is in bed or not
-int threshold = 200;
+int threshold = 80;
 
 // this tells us if the person is in
 // bed or not
-int in_bed = 0;
+boolean in_bed = false;
 
 void setup() {
   Spark.variable("pressure", &pressure, INT);
-  Spark.variable("in_bed", &in_bed, INT);
+  Spark.variable("in_bed", &in_bed, BOOLEAN);
 }
 
 void loop() {
 
-  pressure = analogRead(A0) / 10;
+  int pre_pressure = analogRead(A0) / 35;
 
   if (pressure > threshold) {
-    in_bed = 1;
+    if (!in_bed) {
+      Spark.publish("alarm/is_in_bed", "true", 60, PRIVATE);
+    }
+    in_bed = true;
   } else {
-    in_bed = 0;
+    if (in_bed) {
+      Spark.publish("alarm/is_in_bed", "false", 60, PRIVATE);
+    }
+    in_bed = false;
   }
 
 }
